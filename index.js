057539e3c482e014016be5fb978564b5777ef32e
Jonathan Rubio <rubiomolina.jonathan@gmail.com>
2026-01-05 12:22:54 -0800

    Document frameworks, instances and static files

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
+// Importing frameworks
 import express from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
 
+// Creating instance for express application, setting port and url
 const app = express();
 const port = 3000;
 const API_URL = "https://pokeapi.co/api/v2/pokemon-species";
 
+// Serving static files and body-parser
 app.use (express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// 
 app.get("/", async(req, res) => {
     try {
         const response = await axios.get(API_URL + "?limit=100000&offset=0");
